c7581fb1e1618de810a9a3572e4c9b5f33d6e787
张浩 <1668471805@qq.com>
2022-01-19 15:47:27 +0800

    插槽传递模板

--- src/components/amap/Amap.vue
+++ src/components/amap/Amap.vue
@@ -1,6 +1,4 @@
 
-************地图模板*************
-
 <template>
   <div class="map-container" v-loading="mapLoading">
     <el-amap
@@ -12,7 +10,19 @@
       :events="amapEvents"
       :expandZoomRange="true"
     >
-      <template></template>
+      <!--运行状态-->
+      <slot name="running"></slot>
+      <!--计划管理-->
+      <slot name="plan"></slot>
+      <!--航线管理-->
+      <slot name="route"></slot>
+      <!--航标航道管理-->
+      <slot name="chanel"></slot>
+      <!--港口泊位管理-->
+      <slot name="port_berth"></slot>
+      <!--航图展示管理-->
+      <slot name="show"></slot>
+
     </el-amap>
 
     <!-- 卫星按钮 -->
@@ -26,11 +36,83 @@
 <script>
 // 引入地图
 import VueAMap from 'vue-amap';
-// 引入地图基本配置
-import mapBase from '@/mixins/amap.base';
+// 引入实例懒加载api
+import { lazyAMapApiLoaderInstance } from 'vue-amap';
 
 export default {
-  mixins: [ mapBase ],
+  data() {
+    // 定义一个变量指向组件实例
+    let self = this;
+    return {
+      // 地图基本参数
+      center: [106.551842, 29.592214],
+      zoom: 12,
+      zooms: [2, 20],
+      mapLoading: false,
+      // 地图实例
+      mapInstance: null,
+      // 事件队列
+      isCompleted: false,
+      completedCallbacks: [],
+      // 卫星图层
+      isSate: false,
+      sateLayer: null,
+      // 地图事件
+      amapEvents: {
+        // 地图初始化
+        init(instance) {
+          lazyAMapApiLoaderInstance.load().then(() => {
+            // 获取地图实例
+            self.mapInstance = instance;
+            // 获取卫星图层
+            self.sateLayer = new AMap.TileLayer.Satellite();
+            // 设置鼠标样式
+            self.mapInstance.setDefaultCursor();
+            // 事件队列依次执行
+            for (let cb of self.completedCallbacks) {
+              cb(self);
+            }
+            self.isCompleted = true;
+          });
+        },
+        // 点击事件
+        click: (e) => {
+          // 获取坐标点
+          console.log(e.lnglat);
+        }
+      }
+    };
+  },
+
+  methods: {
+    // 设置卫星图层
+    setSateLayer() {
+      console.log('设置卫星地图')
+      if (this.mapInstance) {
+        this.isSate = !this.isSate;
+        if (this.isSate) {
+          this.mapInstance.add(this.sateLayer);
+        } else {
+          this.mapInstance.remove(this.sateLayer);
+        }
+      } else {
+        this.$message.error('网速较慢，地图未获取到，请稍后再试');
+      }
+    },
+    // 在地图执行成功后才会去执行，没有成功将函数保存在数组中，等待地图成功后执行数组中的函数
+    onCompleted(cb) {
+      if (this.isCompleted) {
+        return cb();
+      } else {
+        this.completedCallbacks.push(cb);
+      }
+    },
+    //自定义鼠标样式图标
+    setCursor(target = 'default') {
+      // 'default' | 'pointer' | 'move' | 'crosshair'
+      this.mapInstance.setDefaultCursor(target);
+    }
+  }
 };
 </script>
 
--- src/pages/plan/Plan.vue
+++ src/pages/plan/Plan.vue
@@ -1,24 +1,133 @@
 <template>
   <div class="container">
-    <PlanMap></PlanMap>
+    <!--表格区域-->
+    <div class="table-part">
+      <el-header>table</el-header>
+      <el-row>
+        <el-col :span="1">&nbsp;</el-col>
+        <el-col :span="10">
+          <el-input placeholder="请输入计划航线关键字">
+            <template slot="append"><i class="el-icon-search"></i></template>
+          </el-input>
+        </el-col>
+        <el-col :span="12" class="col-btn">
+          <el-button type="warning">添加航线</el-button>
+        </el-col>
+      </el-row>
+      <MyTable :tableData="tableData"></MyTable>
+    </div>
+
+    <!--地图-->
+    <PlanMap>
+      <template #plan>
+        <!--坐标点-->
+        <template>
+          <el-amap-marker :position="[106.556342, 29.592314]" :draggable="true" :offset="offset">
+            <div class="marker" @click="clickPoint">
+              <p>3</p>
+            </div>
+          </el-amap-marker>
+
+          <el-amap-marker :position="[106.551342, 29.592314]" :offset="offset">
+            <div class="precise-marker" @click="clickPoint">
+              <p>23</p>
+            </div>
+          </el-amap-marker>
+        </template>
+        <!--计划航线-->
+        <template>
+          <el-amap-polyline :path="[[106.556342, 29.592314], [106.551342, 29.592314]]"> </el-amap-polyline>
+        </template>
+      </template>
+    </PlanMap>
   </div>
 </template>
 
 <script>
 // 导入地图组件
-import PlanMap from 'components/amap/PlanMap';
+import PlanMap from 'components/amap/Amap';
+// 导入表格
+import MyTable from 'components/common/table/Mytable';
 
 export default {
   components: {
-    PlanMap
+    PlanMap, MyTable
+  },
+  data () {
+    return {
+      // 表格数据
+      tableData: [],
+
+      /********************* 地图相关 *********************/
+      // marker偏移量
+      offset: [-16, -31]
+    }
+  },
+  methods: {
+    clickPoint() {
+      console.log('点击了坐标点!');
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+@elColor: #409eff;
+@markerPath: "../../assets/img/map/marker.png";
+@preciseMarkerPath: "../../assets/img/map/precise-marker.png";
+@markerSize: 32px;
+
 .container {
   padding: 0 !important;
+  position: relative;
   width: 100%;
   height: 100%;
+}
+
+/***************表格区域***************/
+.table-part {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 600px;
+  height: 400px;
+  z-index: 1;
+  background-color: #e7e7e7;
+  .el-header {
+    padding: 0;
+    height: 30px !important;
+    text-align: center;
+  }
+  /*search input*/
+  /deep/ .el-input-group__append,
+  .el-input-group__prepend {
+    background-color: @elColor;
+    color: #fafafa;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .col-btn {
+    direction: rtl;
+  }
+}
+
+/*************** 地图相关 ***************/
+/* 坐标点 */
+.marker, .precise-marker{
+  position: relative;
+  width: @markerSize;
+  height: @markerSize;
+  background: url(@markerPath);
+  p {
+    display: inline-block;
+    width: 100%;
+    font-size: 12px;
+    color: #fafafa;
+    text-align: center;
+  }
+}
+.precise-marker {
+  background: url(@preciseMarkerPath);
 }
 </style>
